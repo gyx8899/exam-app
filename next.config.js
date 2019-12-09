82c900636ec09347973ca6ea4f8f9dc46e55957c
Steper Kuo <gyx8899@126.com>
2019-12-09 23:17:26 +0800

    [Mod] Revert assetPrefix change;

--- next.config.js
+++ next.config.js
@@ -23,7 +23,6 @@ const themeVariables = {
 }
 
 module.exports = withLess({
-	assetPrefix: process.env.NODE_ENV === 'production' ? '/exam-app' : '',
 	lessLoaderOptions: {
 		javascriptEnabled: true,
 		modifyVars: themeVariables, // make your antd custom effective
