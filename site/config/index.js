e93ee1749c3b0603aea1e46886b4792bc72f1ce1
Steper Kuo <gyx8899@126.com>
2020-04-13 13:57:16 +0800

    feature(index): Update meta info for SEO

--- site/config/index.js
+++ site/config/index.js
@@ -1,6 +1,6 @@
 export const Title = '考试 A+ 助手';
-export const MetaDescription = '考试 A+ 助手是一款辅助用户练习考题的应用，支持离线使用，';
-export const MetaKeyWord = '考试，练习，助手，应用，excel';
+export const MetaDescription = '《考试 A+ 助手》，一款基于 Next.js 的 考试 A+ 助手应用，通过使用 excel 文件形式的考题文件，转换成 JSON 数据，展现在考题页面，形成一个供用户练习考题的应用。';
+export const MetaKeyWord = '考试，考试题库，练习，考试练习，助手，应用，excel，2020，金护帽，消防测试';
 export const MetaAuthor = 'Steper Kuo';
 
 export const GA_TRACKING_ID  = 'G-H29X5PLPDJ';
