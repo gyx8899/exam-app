a8c07393b167841f7546d1eca14dfdf41b522d60
Steper Kuo <gyx8899@126.com>
2021-04-15 00:00:27 +0800

    refactor(title): Update exam title

--- site/data/index.js
+++ site/data/index.js
@@ -1,36 +1,36 @@
 import { convertJSON as exam201907 } from './tool/Exam-201907';
 import { convertJSON as jinHuMao } from './tool/JinHuMao';
 import { convertJSON as xiaoFangTiKu } from './tool/XiaoFangTiKu';
-import { convertJSON as exam20200819 } from './tool/Exam-20200819';
+import { convertJSON as exam202008 } from './tool/Exam-20200819';
 import { convertJSON as exam202104 } from './tool/Exam-202104';
 import { convertJSON as exam202104SystemLedger } from './tool/Exam-202104';
 
 let _examConfig = [
 	{
 		id: 'Exam-202104-SystemLedger',
-		title: '202104-系统台账',
+		title: '202104-系统台账-试题',
 		convertJSON: exam202104SystemLedger,
 		isLatest: true,
 	},
 	{
 		id: 'Exam-202104',
-		title: '月考-202104',
+		title: '202104-月考-试题',
 		convertJSON: exam202104,
 		isLatest: true,
 	},
 	{
 		id: 'Exam-20200819-1',
-		title: '月考-20200819-1',
-		convertJSON: exam20200819,
+		title: '202008-月考-试题-1',
+		convertJSON: exam202008,
 	},
 	{
 		id: 'Exam-20200819-2',
-		title: '月考-20200819-2',
-		convertJSON: exam20200819,
+		title: '202008-月考-试题-2',
+		convertJSON: exam202008,
 	},
 	{
 		id: 'Exam-201907',
-		title: '月考-201907',
+		title: '201907-月考-试题',
 		convertJSON: exam201907,
 	},
 	{
