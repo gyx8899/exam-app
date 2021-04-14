ca74a2388f7f7a15d92415f401a36cbae7e7a8f7
Steper Kuo <gyx8899@126.com>
2021-04-14 23:38:00 +0800

    feat(about): Update about with email link.

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,19 @@
-import React from "react";
-import {Empty} from "antd";
+import React from 'react';
+import { Result, Button } from 'antd';
 
 const About = () => {
 	return (
-			<Empty />
+		<Result
+			status="success"
+			title="谨此献给我亲爱的老婆-平平!"
+			extra={
+				<div>
+					欢迎反馈：
+					<a href="mailto:gyx8899@126.com">gyx8899@126.com</a>
+				</div>
+			}
+		/>
 	);
 };
 
-export default About
+export default About;
